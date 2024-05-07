06149634840fbf1ebf31eac75831d18dd1be859a
Eleaha <ellieharrington8@gmail.com>
2024-05-07 14:51:49 +0100

    adds an individual article and loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import Articles from './components/Articles';
 import Topic from './components/Topic';
 import Article from './components/Article';
 import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+
+	const [isLoading, setIsLoading] = useState(true)
+
 	return (
 		<main>
 			<Header />
 			<Routes>
-				<Route path="/" element={<Articles />} />
+				<Route
+					path="/"
+					element={
+						<Articles setIsLoading={setIsLoading} isLoading={isLoading} />
+					}
+				/>
 				<Route path="/topic" element={<Topic />} />
-				<Route path="/article/:article_id/" element={<Article />} />
+				<Route
+					path="/article/:article_id/"
+					element={
+						<Article setIsLoading={setIsLoading} isLoading={isLoading} />
+					}
+				/>
 				<Route />
 			</Routes>
 		</main>
